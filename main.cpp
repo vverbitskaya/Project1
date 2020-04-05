e973bc116293bec70ee441cfd8d95cc56467b743
vverbitskaya <verbitsckaya.veronika@yandex.ru>
2020-04-05 20:53:36 +0300

    Implement linked list node

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+class LinkedListNode {
+    LinkedListNode* next;
+    LinkedListNode* prev;
+}
+
+class LinkedList {
+}
+
 int main() {
     int a = 10;
     for(int i = 0; i < a; ++i) {
